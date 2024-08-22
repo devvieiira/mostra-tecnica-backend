3219930598aef36a0398e11287b9fa9e6b717d88
devvieiira <kauavieira900@gmail.com>
2024-08-22 11:36:01 -0300

    [WIP] [feat]: add delete function

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,6 +2,7 @@ import fastify from "fastify";
 import cors from "@fastify/cors";
 import { ImportAvaliador } from "./routes/avaliador/create";
 import { getAvaliadores } from "./routes/avaliador/find";
+import { deleteAvaliador } from "./routes/avaliador/delete";
 // import multer from "fastify-multer";
 // import { ReadFile } from "./routes/avaliador/readFile";
 
@@ -17,6 +18,7 @@ app.register(cors, {
 // app.register(ReadFile);
 app.register(ImportAvaliador);
 app.register(getAvaliadores);
+app.register(deleteAvaliador);
 
 app.listen({ port: 4000, host: "0.0.0.0" }).then((value) => {
 	console.log("HTTP Server Running!", value);
